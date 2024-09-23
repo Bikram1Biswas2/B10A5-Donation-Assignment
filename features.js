b259567b556bb8847eb1b5fb50f6540e1a98bb8f
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-09-23 17:56:01 +0530

    Card section all functionality done

---
+++ features.js
@@ -0,0 +1,15 @@
+// Blog-page
+document.getElementById('btn-blog').addEventListener('click',function(){
+    
+    window.location.href='./blog.html'
+})
+
+// Donation Page
+document.getElementById('btn-donation-section').addEventListener('click',function(){
+
+    navigateToPage('./index.html')
+})
+
+
+// Donate money Value
+
--- utilities.js
+++ utilities.js
@@ -1,9 +1,51 @@
-function showElementsById(id){
-
-    // hide all the section
-    document.getElementById('donation-card').classList.add('hidden')
-
-
-    // show the section with the provide id as a parameter
-    
+// common function for redirect page
+function navigateToPage(url){
+    window.location.href= url;
 }
+
+
+// common function for get Input Value
+function getInputField(id){
+   
+    const inputValue = document.getElementById(id).value;
+    const inputNumber = parseFloat(inputValue);
+    if(isNaN(inputValue) || inputValue <=0){
+        alert('please enter a valid amount');
+        return 0;
+    }
+    return inputNumber;
+}
+
+// Function to handle the donation
+
+function handleDonate(inputFieldId, cardBalanceId){
+
+    // get the donation amount
+    const donationAmount = getInputField(inputFieldId);
+    if(donationAmount === 0)
+    return;
+
+    // main balance
+const mainBalanceElement = document.getElementById('main-balance');
+
+let mainBalance = parseFloat(mainBalanceElement.innerText)
+
+if(donationAmount > mainBalance){
+    alert('Insufficient Balance for donation')
+    return;
+}
+
+mainBalance -= donationAmount
+mainBalanceElement.innerText = mainBalance.toFixed(2)
+
+const cardBalanceEl = document.getElementById(cardBalanceId)
+
+let currentCardBalance = parseFloat(cardBalanceEl.innerText);
+currentCardBalance += donationAmount;
+cardBalanceEl.innerText = currentCardBalance.toFixed(2)
+
+// Clear the input field
+document.getElementById(inputFieldId).value = '';
+
+}
+
