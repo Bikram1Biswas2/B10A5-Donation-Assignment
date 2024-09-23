b828fd661a79cfd1b49e2c1575f0e15cf83bda84
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-09-23 12:31:19 +0530

    Blog button functionality added

---
+++ utilities.js
@@ -0,0 +1,9 @@
+function showElementsById(id){
+
+    // hide all the section
+    document.getElementById('donation-card').classList.add('hidden')
+
+
+    // show the section with the provide id as a parameter
+    
+}
